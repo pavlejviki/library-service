43f45402805008d69c3d63f7a70b81eb890e900a
Viktoria Pavlej <viktorialondon16@gmail.com>
2023-01-09 11:50:37 +0000

    Create endpoint for user registration

--- users_service/urls.py
+++ users_service/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from users_service.views import CreateUserView
+
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
 ]
--- users_service/views.py
+++ users_service/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
 
+from users_service.serializers import UserSerializer
+
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
