79302765370fa4246f115085e69bab454d2d7f97
Viktoria Pavlej <viktorialondon16@gmail.com>
2023-01-09 12:37:38 +0000

    Add endpoint for retrieving and editing user profile info

--- users_service/views.py
+++ users_service/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from users_service.serializers import UserSerializer
 
@@ -10,8 +12,7 @@ class CreateUserView(generics.CreateAPIView):
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
-    # authentication_classes = (TokenAuthentication,)
-    # permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
