fbdcbf5762b57ab97d6b88496a94619110bf6657
Viktoria Pavlej <viktorialondon16@gmail.com>
2023-01-09 12:09:13 +0000

    Add djangorestframework-simplejwt to requirements.txt

--- users_service/urls.py
+++ users_service/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from users_service.views import CreateUserView
+from users_service.views import CreateUserView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "users_service"
--- users_service/views.py
+++ users_service/views.py
@@ -6,3 +6,12 @@ from users_service.serializers import UserSerializer
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    # authentication_classes = (TokenAuthentication,)
+    # permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
